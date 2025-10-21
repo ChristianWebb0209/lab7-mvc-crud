3639c58e93dfbde3e72b0a05be7df20f0318d39b
Christian Webb <cawebb22@gmail.com>
2025-10-20 18:35:16 -0700

    created barebones chatController

---
+++ src/js/controller.js
@@ -0,0 +1,9 @@
+import { ChatModel } from './model.js';
+import { ChatView } from './view.js';
+import { getBotResponse } from './eliza.js';
+
+// controller coordinates between model and view
+export class ChatController {
+    constructor() { }
+
+}

60b2ead42f5b2d164830a4b50147af465d07e901
Christian Webb <cawebb22@gmail.com>
2025-10-22 17:45:38 -0700

    added unfinished controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,8 +2,73 @@ import { ChatModel } from './model.js';
 import { ChatView } from './view.js';
 import { getBotResponse } from './eliza.js';
 
-// controller coordinates between model and view
 export class ChatController {
-    constructor() { }
+    constructor() {
+        this.model = new ChatModel();
+        this.view = new ChatView(this);
 
+        this.view.update(messages);
+        // sat up view at first with current messages
+        this.view.update(this.model.messages);
+    }
+
+    handleSendMessage() { }
+
+    handleEditMessage(messageId) { }
+
+    handleSaveEdit(messageId, newText) { }
+
+    // Handle canceling edit
+    handleCancelEdit(messageId) {
+        this.view.editingMessageId = null;
+        this.view.update(this.model.messages);
+    }
+
+
+
+    handleExport() {
+        const data = this.model.exportData();
+        const filename = `export-${new Date().toISOString()}.json`;
+
+        this.view.downloadFile(data, filename);
+    }
+
+    handleImport() {
+        this.view.fileInput.click();
+    }
+
+    handleFileSelected(file) { }
+
+    // Handle deleting a message
+    handleDeleteMessage(messageId) {
+        const message = this.model.getMessage(messageId);
+        if (!message) {
+            this.view.showError('error deleting message');
+            return;
+        }
+
+        this.view.showConfirmationAlert(
+            'are you sure you want to delete?',
+            () => { }, // do nothign if canceled
+            () => {
+                const deleted = this.model.deleteMessage(messageId);
+                if (deleted)
+                    this.view.showSuccess('message deleted');
+                else
+                    this.view.showError('error deleting message');
+            }
+        );
+    }
+
+    // clear all messages
+    handleClear() {
+        this.view.showConfirmationAlert(
+            `are you sure you want to delete all ${this.model.getMessageCount()} messages? no backsies!`,
+            () => { },
+            () => {
+                this.model.deleteAllMessages();
+                this.view.update(this.model.messages);
+            }
+        );
+    }
 }
